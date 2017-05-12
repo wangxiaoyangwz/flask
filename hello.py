3325f94617858f0c9c7c2768b02ba86c69c52d7f
wangxiaoyangwz <1161652587@qq.com>
2017-05-12 22:16:20 +0800

    add Manager

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_script import Manager
+
 app=Flask(__name__)
+manager=Manager(app)
 
 @app.route('/')
 def index():
@@ -9,4 +12,4 @@ def user(name):
     return '<h1>hello %s!<h1>'% name
 
 if __name__=='__main__':
-    app.run(debug=True)
+    manager.run()
