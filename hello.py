44febb8a5f387dceebd64f79cad6ec9f76f1fa6f
wangxiaoyangwz <1161652587@qq.com>
2017-05-13 16:19:18 +0800

    navbar

--- hello.py
+++ hello.py
@@ -1,15 +1,18 @@
-from flask import Flask
+from flask import Flask,render_template
+from flask_bootstrap import Bootstrap
 from flask_script import Manager
+
 
 app=Flask(__name__)
 manager=Manager(app)
+bootstrap=Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>hello world!</h1>'
+    return render_template('index.html')
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>hello %s!<h1>'% name
+    return render_template('user.html',name=name)
 
 if __name__=='__main__':
     manager.run()
