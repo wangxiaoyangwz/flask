b87c1d2222e6148d53a6abaa0e097e731423f707
wangxiaoyangwz <1161652587@qq.com>
2017-05-13 21:52:39 +0800

    methods/form.name.data/

--- hello.py
+++ hello.py
@@ -1,23 +1,32 @@
+# -*- coding: utf-8 -*-
 from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
 from flask_script import Manager
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
 
 
 app=Flask(__name__)
 manager=Manager(app)
 bootstrap=Bootstrap(app)
 moment=Moment(app)
+app.config['SECRET_KEY']='hard to guess string'
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+@app.route('/',methods=['GET','POST'])#告诉flaskzei URL 映射中index注册为GET/POST的请求的处理程序
 def index():
-    return render_template('index.html',current_time=datetime.utcnow())
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html',name=name)
-
+	name=None #存放表单输入name有效名字
+	form=NameForm()#实例表示表单 创建表单
+	if form.validate_on_submit():#测试函数 True
+		name=form.name.data#用户输入名字 用date获取
+		form.name.data=''#清空表单字段
+	return render_template('index.html',form=form,name=name)
 
 @app.errorhandler(500)
 def internal_server_error(e):
