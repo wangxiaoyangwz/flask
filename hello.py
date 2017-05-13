fa660dffbac7927a9dfdb7226e85f727ff370064
wangxiaoyangwz <1161652587@qq.com>
2017-05-13 22:16:13 +0800

    session['name']/redirect/

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask,render_template
+from flask import Flask,render_template,session,redirect,url_for
 from flask_bootstrap import Bootstrap
 from flask_script import Manager
 from flask_moment import Moment
@@ -19,15 +19,6 @@ class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
-@app.route('/',methods=['GET','POST'])#告诉flaskzei URL 映射中index注册为GET/POST的请求的处理程序
-def index():
-	name=None #存放表单输入name有效名字
-	form=NameForm()#实例表示表单 创建表单
-	if form.validate_on_submit():#测试函数 True
-		name=form.name.data#用户输入名字 用date获取
-		form.name.data=''#清空表单字段
-	return render_template('index.html',form=form,name=name)
-
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'),500
@@ -35,6 +26,15 @@ def internal_server_error(e):
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'),404
+
+@app.route('/',methods=['GET','POST'])#告诉flaskzei URL 映射中index注册为GET/POST的请求的处理程序
+def index():
+	form=NameForm()#实例表示表单 创建表单
+	if form.validate_on_submit():#测试函数 True
+		session['name']=form.name.data#用户输入名字 用date获取  session[]字典存储name 用户会话
+		return redirect(url_for('index'))#生成http重定向响应 参数实际是‘/’
+		                                  #
+	return render_template('index.html',form=form,name=session.get('name'))#字典获取name
 
 
 if __name__=='__main__':
