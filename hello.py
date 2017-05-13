8dab3e40caf70ae283343e0a521cb23c69cdfc21
wangxiaoyangwz <1161652587@qq.com>
2017-05-13 16:48:46 +0800

    error

--- hello.py
+++ hello.py
@@ -10,9 +10,20 @@ bootstrap=Bootstrap(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html',name=name)
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
 
 if __name__=='__main__':
     manager.run()
